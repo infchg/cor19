c88af298bd65a5087e7b5ce9d0349d7c39454c7a
Juan Carlos lap <J.C.Martinez-alba@iaea.org>
2020-04-05 22:06:56 +0200

    apr4

--- jobs/chartjs.rb
+++ jobs/chartjs.rb
@@ -1,7 +1,7 @@
-SCHEDULER.every "2s" do |job|
+SCHEDULER.every "2h" do |job|
   send_event("chart", {
   	type: "bar",
-  	header: "Calories Burned",
+  	header: "C Burned",
   	labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
   	colorNames: ["yellow", "yellow", "yellow", "yellow", "yellow"],
   	datasets: [210, 339, 220, 494, 109]
