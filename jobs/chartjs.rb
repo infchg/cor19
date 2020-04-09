66376acc0f7ae10a8e88b0bd9fc9775381fdcafc
Juan Carlos lap <J.C.Martinez-alba@iaea.org>
2020-04-10 00:14:34 +0200

    apr9-fixedtypo

--- jobs/chartjs.rb
+++ jobs/chartjs.rb
@@ -13,5 +13,6 @@ SCHEDULER.every "2m" do |job|
   	labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
   	colorNames: ["yellow", "yellow", "yellow", "yellow", "yellow"],
   	datasets: [21, 33, 22, 49, 10]
+  })
 
 end
