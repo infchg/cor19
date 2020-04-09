1ae148a61c9814eac803b5ccbb88b570b33379bf
Juan Carlos lap <J.C.Martinez-alba@iaea.org>
2020-04-10 00:01:45 +0200

    apr9

--- jobs/chartjs.rb
+++ jobs/chartjs.rb
@@ -1,9 +1,17 @@
-SCHEDULER.every "2h" do |job|
-  send_event("chart", {
+SCHEDULER.every "2m" do |job|
+  send_event("chart3", {
   	type: "bar",
   	header: "C Burned",
   	labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
   	colorNames: ["yellow", "yellow", "yellow", "yellow", "yellow"],
-  	datasets: [210, 339, 220, 494, 109]
+  	datasets: [21, 33, 22, 49, 10]
   })
+
+  send_event("bar-chart", {
+  	type: "bar",
+  	header: "C Burned",
+  	labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
+  	colorNames: ["yellow", "yellow", "yellow", "yellow", "yellow"],
+  	datasets: [21, 33, 22, 49, 10]
+
 end
