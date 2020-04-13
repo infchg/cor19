02f72c5095075743decad99d67ff8fb88971d012
infchg <infchg@github.com>
2020-04-13 13:55:26 +0200

    code-hist.rb

--- jobs/hist.rb
+++ jobs/hist.rb
@@ -1,4 +1,13 @@
+require 'sinatra'
+use Rack::Logger
+
+set :logger, Logger.new(STDOUT) # STDOUT & STDERR is captured by unicorn
+logger.info('starting hi info')
+
 get '/fi' do
-  #File.read(File.join('public', 'index.html'))
+  # File.read(File.join('public', 'index.html'))
+  logger.info('query hi info')
   settings.history.to_yaml
 end
+
+File.write('assets/stylesheets/hi.css', settings.history.to_yaml)
