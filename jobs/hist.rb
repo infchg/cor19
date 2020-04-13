6d72d432b07e05c1089bc71969118f09fd1feefb
infchg <infchg@github.com>
2020-04-13 14:10:16 +0200

    code-hist.rb-get-here-notused

--- jobs/hist.rb
+++ jobs/hist.rb
@@ -2,12 +2,20 @@ require 'sinatra'
 use Rack::Logger
 
 set :logger, Logger.new(STDOUT) # STDOUT & STDERR is captured by unicorn
-puts('starting hi info')
 
-get '/fi' do
+=begin
+#NO ESTO CRE NEW SRVR get '/fi' do
   # File.read(File.join('public', 'index.html'))
   puts('query hi info')
   settings.history.to_yaml
+=end
+
+after do
+  puts "jc - resp"
+  puts response.status
 end
 
-File.write('assets/stylesheets/hi.css', settings.history.to_yaml)
+puts('jc starting hi info')
+puts( settings.history.to_yaml)
+#File.write('assets/stylesheets/hi.css', settings.history.to_yaml)
+#puts('jc starting hi info')
