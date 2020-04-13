5d32b715e0742547c38feada055321b9b71b1016
infchg <infchg@github.com>
2020-04-13 13:59:41 +0200

    code-hist.rb-puts

--- jobs/hist.rb
+++ jobs/hist.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 use Rack::Logger
 
 set :logger, Logger.new(STDOUT) # STDOUT & STDERR is captured by unicorn
-Logger.info('starting hi info')
+puts('starting hi info')
 
 get '/fi' do
   # File.read(File.join('public', 'index.html'))
-  Logger.info('query hi info')
+  puts('query hi info')
   settings.history.to_yaml
 end
 
