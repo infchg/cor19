29060aa1cb374bee90df0dd9592e6350ca563aed
infchg <infchg@github.com>
2020-04-16 10:39:11 +0200

    April update 04-15

--- bin/change.sh
+++ bin/change.sh
@@ -2,7 +2,7 @@
 
 git show fe5290:dashboards/covid19.erb  | sed -E -f change.sed  >  ../dashboards/covid19.erb 
 
- sed -E -f change.sed  bas > ../jobs/covidzoom.rb 
+ sed -E -f change.sed  TEPL-covidzoom.rb > ../jobs/covidzoom.rb 
 
 git commit -am "April update $(date  +%m-%d -d -1day) "
 git diff --stat --cached origin/master
